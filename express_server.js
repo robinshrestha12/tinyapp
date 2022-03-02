d6452c9a2d83168cb08841016636ec3ec327fc4b
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-03-02 14:38:48 -0500

    Url shortening Part II complete

--- express_server.js
+++ express_server.js
@@ -37,6 +37,14 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => { //short Url with full url redirect
+  if (urlDatabase[req.params.shortURL]) {
+    const fullLongURL = urlDatabase[req.params.shortURL];
+    res.redirect(fullLongURL);
+  } else {
+    res.sendStatus(404);
+  }
+});
 // app.get("/set", (req, res) => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
@@ -46,13 +54,9 @@ app.get("/urls/:shortURL", (req, res) => {
 //   res.send(`a = ${a}`);
 // });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  const rndShort = generateRandomString();
-  urlDatabase[rndShort] = {
-    shortURL: rndShort,
-    longURL: req.body.longURL
-  };
+  const rndShort = generateRandomString(); //generating random alphaneumeric string
+  urlDatabase[rndShort] =  req.body.longURL; //setting new long url with key
+  res.redirect(`/urls/${rndShort}`); //redirecting to short URL
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
