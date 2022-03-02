26386e5ce250bc69e0ccc53bc0ce66d8829502da
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-03-01 22:12:22 -0500

    Second Route

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get("/urls", (req, res) => {
 const templateVars = {urls: urlDatabase };
 res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
